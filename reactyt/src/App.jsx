5e8b36dd6eaa3876b91267c7d1e633738af96557
mdMERAJ77 <mmd200975@gmail.com>
2024-10-08 12:06:13 +0530

    Add .gitattributes for line ending normalization

--- reactyt/src/App.jsx
+++ reactyt/src/App.jsx
@@ -1,19 +1,31 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+
+// const App = () => {
+//   const [num, setNum] = useState(0)
+//   function increment(){
+//     setNum(num+1)
+//   }
+//   return (
+//     <div>
+//       <h1>Number is: {num}</h1>
+//       <button onClick={increment}>increment</button>
+//       <button onClick={()=>{setNum(num-1)}}>decrement</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
 
 const App = () => {
-  const [num, setNum] = useState(0)
   return (
     <div>
-      <h2>Number is: {num}</h2>
-      <button onClick={
-        function(){setNum(num+1)}
-      }>increment</button>
-
-      <button onClick={()=>{
-        setNum(num-1)
-      }}>decrement</button>
+      <h1 className='text-5xl bg-red-700'>hello</h1>
+      <h2>thanks</h2>
     </div>
   )
 }
 
 export default App
+
